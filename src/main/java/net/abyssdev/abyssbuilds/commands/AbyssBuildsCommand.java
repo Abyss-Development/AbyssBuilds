e8f4bdda61a77c8b12c38c955547e1cb7bdc60eb
sysdm-del <timothy-berg@outlook.com>
2022-07-08 16:58:48 +0200

    fix help cmd

--- src/main/java/net/abyssdev/abyssbuilds/commands/AbyssBuildsCommand.java
+++ src/main/java/net/abyssdev/abyssbuilds/commands/AbyssBuildsCommand.java
@@ -41,7 +41,7 @@ public class AbyssBuildsCommand implements CommandExecutor {
             , ColorUtil.color("&3&l» &b/abyssbuilds addworld <player> <name/seed>")
             , ColorUtil.color("&3&l» &b/abyssbuilds removeworld <player> <name/see>")
             , ColorUtil.color("&3&l» &b/abyssbuilds settype <player <type>")
-            , ColorUtil.color("&3&l» &b/abyssbuilds drawborder ")
+            , ColorUtil.color("&3&l» &b/abyssbuilds drawborder")
             , ColorUtil.color(" ")
             , ColorUtil.color("&3&m------------------------------")
     ).toArray(new String[0]);
